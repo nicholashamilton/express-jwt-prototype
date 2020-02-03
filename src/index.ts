bf32476044facbf2a16b7646ad572e24c3bfb041
nicholashamilton <nicholashamilton.co@gmail.com>
2020-02-03 00:56:21 -0500

    Update env port variable

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 import App from './app';
 
-const AppInstance = new App(process.env.SERVER_PORT, process.env.DATABASE_URI);
+const AppInstance = new App(process.env.PORT, process.env.DATABASE_URI);
 AppInstance.init();
