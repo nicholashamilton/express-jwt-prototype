f3e55e7d3486d0034bfea6b0ef726cbd5e06a19b
Nick Hamilton <nicholashamilton.co@gmail.com>
2020-02-08 00:54:48 -0500

    Add example.env & makes use of bearer header

--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -4,9 +4,10 @@ import { IUserAuthRequest } from '../../global';
 import { IUser } from '../../user';
 
 export default (req: IUserAuthRequest, res: Response, next: NextFunction) => {
-    const token = req.headers['x-access-token'] as string;
+    const token = req.headers.authorization as string;
     if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, (err: jwt.VerifyErrors, decoded: any) => {
+        const newToken = token.split(' ')[1];
+        jwt.verify(newToken, process.env.JWT_SECRET, (err: jwt.VerifyErrors, decoded: any) => {
             if (err) {
                 return res.status(401).json({
                     success: false,
